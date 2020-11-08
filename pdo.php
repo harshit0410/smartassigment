2027d853473a4f853001623e2d9dc341fd28c319
Harshit Singhal <41239446+harshit0410@users.noreply.github.com>
2020-11-08 18:06:45 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=student', 
-   'root', '12345');
+   'root', '-----');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
